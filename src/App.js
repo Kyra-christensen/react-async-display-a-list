d63dc81487ca85dba2f21edb9519ad03b5f03aaa
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 01:26:01 -0800

    imported getPlanets, useState, useEffect functions and set state for planets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 // import your arrays here
+import { getPlanets } from './services/fetch-utils';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
