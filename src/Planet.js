5a284327dfb19f87006730a3cc9ba6dca9de4734
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 02:11:58 -0800

    added text to the planet p tags

--- src/Planet.js
+++ src/Planet.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Planet({ planet }) {
   return <div>
-    <p>{planet.name}</p>
-    <p>{planet.radius}</p>
-    <p>{planet.distance_from_sun}</p>
-    <p>{planet.surface_area}</p>
+    <p>Name: {planet.name}</p>
+    <p>Radius: {planet.radius}</p>
+    <p>Distance from the sun:{planet.distance_from_sun}</p>
+    <p>Surface area:{planet.surface_area}</p>
   </div>;
 }
 
