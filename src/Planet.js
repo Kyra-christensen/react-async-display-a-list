1833bda5ffabbb6c5fd57c5eb0542e1d04ba02d6
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 01:52:09 -0800

    created planet function that renders a single planet

--- src/Planet.js
+++ src/Planet.js
@@ -1,1 +1,11 @@
+import React from 'react';
+
+export default function Planet({ planet }) {
+  return <div>
+    <p>{planet.name}</p>
+    <p>{planet.radius}</p>
+    <p>{planet.distance_from_sun}</p>
+    <p>{planet.surface_area}</p>
+  </div>;
+}
 
