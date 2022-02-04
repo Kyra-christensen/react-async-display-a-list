7554655bbd7303dca90edbc44ddedc8f60b13486
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 04:02:43 -0800

    added images to the flower components

--- src/Flower.js
+++ src/Flower.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Native from './Native';
+// import Native from './Native';
 export default function Flower({
   name,
   family,
   kingdom,
-  native
+  native,
+  img
 }) {
   return <div className='flower'>
     <p>This is a {name}, they belong to the {family} family and to the {kingdom} kingdom and are native to these areas {native}
       {/* <Native native={native}/> */}
     </p>
+    <img src={img} /> 
   </div>;
 }
--- src/FlowersList.js
+++ src/FlowersList.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Flower from './Flower';
 
 export default function FlowersList({ flowers }) {
-  return flowers.map((flower, i) => <Flower key={`${flower}-${i}`} name={flower.name} family={flower.family} kingdom={flower.kingdom} native={flower.native_to} />);
+  return flowers.map((flower, i) => <Flower key={`${flower}-${i}`} name={flower.name} family={flower.family} kingdom={flower.kingdom} native={flower.native_to} img={flower.img}/>);
 }
 
