684f1a6a935fcde839c199071f91ecd0e99bbd00
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 09:09:29 -0800

    fixed test, now passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Planets:', async () => {
+test('renders This is Floofy:', async () => {
   render(<App />);
   const linkElement = await screen.findByText(/This is Floofy/i);
   expect(linkElement).toBeInTheDocument();
